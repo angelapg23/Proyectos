07aa35f8f5dca2c1d3854c1651bb236655e6d15e
angelapg23 <anpg14.ap@gmail.com>
2019-07-22 12:54:11 -0600

    Agregando clase Ciudad y cliente

---
+++ proyecto1/Cliente.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Cliente
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; }
+    public int Telefono { get; set; }
+    public string Ciudad { get; set; }
+    public string Direccion { get; set; }
+
+
+}
+
--- proyecto1/proyecto1/Form1.cs
+++ proyecto1/proyecto1/Form1.cs
@@ -19,7 +19,46 @@ namespace proyecto1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Vive como si fuera tu ultimo dia");
+            
+            var clientes1 = new Cliente();
+            clientes1.Id = 1;
+            clientes1.Nombre = "Angela";
+            clientes1.Ciudad = "El progreso";
+
+            Cliente clientes2 = new Cliente();
+            clientes2.Id = 2;
+            clientes2.Nombre = "Jose";
+            clientes2.Ciudad = "San pedro sula";
+
+            
+            var listadeclientes = new List<Cliente>();
+            listadeclientes.Add(clientes1);
+            listadeclientes.Add(clientes2);
+
+            foreach (var c in listadeclientes)
+            {
+                MessageBox.Show(c.Nombre);
+                MessageBox.Show(c.Ciudad);
+
+
+
+
+            }
+
         }
     }
+
+    public class Cliente
+    {
+
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int Telefono { get; set; }
+        public string Ciudad { get; set; }
+        public string Direccion { get; set; }
+    }
 }
+    
+
+    
+
