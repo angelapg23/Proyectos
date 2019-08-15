e86af8443ff3e49eac18e7c336265b3d8d220aca
angelapg23 <anpg14.apQ>
2019-08-15 10:37:42 -0600

    Creando herencia con las clases personajes, superpoderes, Villano y Heroe

---
+++ programa herencia/programa herencia/Personajes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa_herencia
+{
+    public class Personajes: SuperPoderes
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
---
+++ programa herencia/programa herencia/SuperPoderes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa_herencia
+{
+    public class SuperPoderes
+    {
+        public string Poder { get; set; }
+
+    }
+}
